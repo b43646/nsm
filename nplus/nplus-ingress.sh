dd4849d9ab1dda5abce26bb0b34504252eef5dcf
b43646 <408514942@qq.com>
2021-06-27 04:30:48 -0400

    update nplus-ingress

--- nplus/nplus-ingress.sh
+++ nplus/nplus-ingress.sh
@@ -10,7 +10,7 @@ kubectl apply -f common/default-server-secret.yaml
 
 kubectl apply -f common/nginx-config.yaml
 
-kubectl apply -f common/ingress-class.yaml
+# kubectl apply -f common/ingress-class.yaml
 
 kubectl apply -f common/crds/k8s.nginx.org_virtualservers.yaml
 
